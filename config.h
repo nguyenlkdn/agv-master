b40ece0b02171328ca50a9308ed683f992aa82a8
Nguyen Truong <nguyenlkdn@gmail.com>
2017-11-27 11:09:21 +0700

    remove redundant files

--- config.h
+++
@@ -1,7 +0,0 @@
-#define ROBOT_DEVICE	"/dev/ttyUSB0"
-#define STATION_DEVICE	"/dev/ttyUSB1"
-uint16_t STATION1_ENABLE   =  1;
-uint16_t STATION2_ENABLE   =  1;
-uint16_t STATION3_ENABLE   =  1;
-uint16_t STATION4_ENABLE   =  1;
-uint16_t STATION5_ENABLE   =  1;
