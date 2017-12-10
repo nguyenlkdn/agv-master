cef8a70309112501d8ba98d61ab3596bca6b68cd
Nguyen Truong <nguyenlkdn@gmail.com>
2017-12-10 14:18:38 +0700

    auto correct sensor status

--- modbus-rtu-server.c
+++ modbus-rtu-server.c
@@ -717,7 +717,7 @@ void *robotThread(void *vargp)
       robot_status = robotRegister_received[0];
       robot_sensor = robotRegister_received[2];
       robotRegister_sent[2] = 0;
-      robotRegister_sent[3] = 0;
+      //obotRegister_sent[3] = 0;
     }
 
     if(robotRegister_received[0] == 0)
